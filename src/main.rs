bafb401a4fe3bc79138b0f08069e84d279954d78
c00136 <c00136@amazingdm.com>
2024-12-17 16:48:47 +0800

    fix(config): 修复git cm命令运行必须依赖Cargo.toml package.metadata.commits配置的问题

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,10 @@ mod git;
 mod questions;
 
 fn run_dialog() -> Option<SurveyResults> {
-    let manifest = parse_manifest().unwrap();
-    if let Some(package) = manifest.package {
-        if let Some(metadata) = package.metadata {
-            // Use default scopes and/or custom ones.
-            let mut types: HashMap<&str, &str> = HashMap::with_capacity(10);
-            if metadata.commits.defaults {
-                types.extend(&*DEFAULT_TYPES);
-            }
+    let mut types: HashMap<&str, &str> = HashMap::with_capacity(10);
+    types.extend(&*DEFAULT_TYPES);
 
-            // Insert custom types.
-            if let Some(custom_types) = &metadata.commits.r#type {
-                for r#type in custom_types.iter() {
-                    types.insert(&r#type.name, &r#type.desc);
-                }
-            }
-
-            return Some(ask(types));
-        } else {
-            eprintln!("Please specify allowed scopes inside of your Cargo.toml file under the `package.metadata.cz` key!");
-        }
-    }
-
-    None
+    return Some(ask(types));
 }
 
 fn create_commit(commit_msg: &str, repo: &Path) {
